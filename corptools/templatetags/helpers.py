43632db4ac77821314dd0868dcfc5e8584971058
AaronKable <aaronkable@gmail.com>
2020-05-24 13:55:38 +0800

    add template tags to this app

---
+++ corptools/templatetags/helpers.py
@@ -0,0 +1,10 @@
+from django.utils.safestring import mark_safe
+from django.template.defaulttags import register
+
+@register.filter(name='addclass')
+def addclass(value, arg):
+    return value.as_widget(attrs={'class': arg})
+
+@register.filter(name='subtract')
+def subtract(value, arg):
+    return value - arg
