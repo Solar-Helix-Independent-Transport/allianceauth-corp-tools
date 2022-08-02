c769618f8704926eaf92b347ca435054195ccede
Aaron Kable <aaronkable@gmail.com>
2022-08-02 19:33:32 +0800

    new django for v3

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -39,15 +39,15 @@ ESI_SSO_CALLBACK_URL = '123'
 
 CACHES = {
     "default": {
-        "BACKEND": "redis_cache.RedisCache",
-        "LOCATION": "localhost:6379",
-        "OPTIONS": {
-            "DB": 1,
-        }
-        # "BACKEND": "django_redis.cache.RedisCache",
-        # "LOCATION": "redis://localhost:6379/1",
+        # "BACKEND": "redis_cache.RedisCache",
+        # "LOCATION": "localhost:6379",
         # "OPTIONS": {
-        #    "COMPRESSOR": "django_redis.compressors.lzma.LzmaCompressor",
+        #    "DB": 1,
         # }
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://localhost:6379/1",
+        "OPTIONS": {
+            "COMPRESSOR": "django_redis.compressors.lzma.LzmaCompressor",
+        }
     }
 }
