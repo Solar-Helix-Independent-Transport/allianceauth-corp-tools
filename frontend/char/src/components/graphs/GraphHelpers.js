84e7b7608bbd0c74269035bf994d656ef6e2bebd
Aaron Kable <aaronkable@gmail.com>
2023-09-25 20:22:11 +0800

    build

--- frontend/char/src/components/graphs/GraphHelpers.js
+++ frontend/char/src/components/graphs/GraphHelpers.js
@@ -2,9 +2,9 @@ export function abbreviateNumber(value) {
   var newValue = value;
   if (value >= 1000) {
     var suffixes = ["", "k", "m", "b", "t"];
-    var suffixNum = Math.floor(("" + value).length / 3);
+    var suffixNum = Math.floor(("" + value).length / 3.1);
     var shortValue = "";
-    for (var precision = 2; precision >= 1; precision--) {
+    for (var precision = 3; precision >= 2; precision--) {
       shortValue = parseFloat(
         (suffixNum != 0 ? value / Math.pow(1000, suffixNum) : value).toPrecision(precision)
       );
