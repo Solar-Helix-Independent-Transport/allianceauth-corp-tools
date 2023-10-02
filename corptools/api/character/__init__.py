c93d1b705e45e5f182c6fc4cb3f2bc138e37ff77
Aaron Kable <aaronkable@gmail.com>
2023-10-02 18:46:15 +0800

    add missing API endpoints

--- corptools/api/character/__init__.py
+++ corptools/api/character/__init__.py
@@ -5,6 +5,7 @@ from .industry import IndustryApiEndpoints
 from .interactions import InteractionApiEndpoints
 from .list import ListApiEndpoints
 from .mining import MiningApiEndpoints
+from .refresh import RefreshApiEndpoints
 from .roles import RolesApiEndpoints
 from .skills import SkillApiEndpoints
 from .status import StatusApiEndpoints
@@ -21,3 +22,4 @@ def setup(api):
     RolesApiEndpoints(api)
     SkillApiEndpoints(api)
     StatusApiEndpoints(api)
+    RefreshApiEndpoints(api)
