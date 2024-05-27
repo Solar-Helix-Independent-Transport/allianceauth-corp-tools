45238be5054b7610bb6109f1dbfbdaec86418275
Aaron Kable <aaronkable@gmail.com>
2024-05-27 18:48:04 +0800

    fix corp build

--- frontend/corp/copy-assets.sh
+++ frontend/corp/copy-assets.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Cleaning old assets."
-rm -rf ../moons/static/moons/assets
-rm ../moons/static/moons/manifest.json
+rm -rf ../../corptools/static/corptools/corp/assets
+rm ../../corptools/static/corptools/corp/manifest.json
 echo "Copying new assets."
-cp build/static/.vite/manifest.json ../moons/static/moons/manifest.json
-cp -r build/static/assets ../moons/static/moons/assets
+cp build/static/.vite/manifest.json ../../corptools/static/corptools/corp/manifest.json
+cp -r build/static/assets ../../corptools/static/corptools/corp/assets
 echo "Assets copied successfully."
