8839c98d2bf38e40eb153eeb20837e43144d89d4
Aaron Kable <aaronkable@gmail.com>
2024-05-30 09:46:56 +0800

    fix minified build

--- frontend/char/vite.config.js
+++ frontend/char/vite.config.js
@@ -21,17 +21,10 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          // creating a chunk to react routes deps. Reducing the vendor chunk size
-          if (id.includes("react-router-dom") || id.includes("react-router")) {
-            return "@react-router";
-          }
-
           if (
-            id.includes("react-query") ||
             id.includes("react-select") ||
             id.includes("javascript-time-ago") ||
-            id.includes("bootstrap") ||
-            id.includes("react-select")
+            id.includes("bootstrap")
           ) {
             return "@libs";
           }
@@ -41,26 +34,6 @@ export default defineConfig({
 
           if (id.includes("amcharts")) {
             return "@charts";
-          }
-
-          if (id.includes("components/char")) {
-            return "@char-components";
-          }
-
-          if (id.includes("components/admin")) {
-            return "@admin-components";
-          }
-
-          if (id.includes("components/graphs")) {
-            return "@graph-components";
-          }
-
-          if (id.includes("components/skills")) {
-            return "@skill-components";
-          }
-
-          if (id.includes("components/")) {
-            return "@components";
           }
         },
       },
