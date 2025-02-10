e6b38cef6ce7789f305b27e28c911c145aa667c4
Aaron Kable <aaronkable@gmail.com>
2025-02-10 19:55:04 +0800

    Add filter to contacts to hide NPCs

--- frontend/frontend/src/Pages/Char/Contacts.tsx
+++ frontend/frontend/src/Pages/Char/Contacts.tsx
@@ -1,12 +1,16 @@
+import { BooleanCheckBox } from "../../Components/BooleanCheckbox";
 import TableWrapper from "../../Components/Tables/BaseTable/TableWrapper";
 import { components } from "../../api/CtApi";
 import { getCharacterContacts } from "../../api/character";
 import { createColumnHelper } from "@tanstack/react-table";
+import { useState } from "react";
+import { Badge, Card, Form } from "react-bootstrap";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 
 const CharacterContacts = () => {
   const { characterID } = useParams();
+  const [showNPC, setShowNPC] = useState(true);
 
   const { data, isFetching } = useQuery({
     queryKey: ["contacts", characterID],
@@ -21,12 +25,25 @@ const CharacterContacts = () => {
     }),
     columnHelper.accessor("contact.name", {
       header: "Contact",
+      cell: (cell) => {
+        return (
+          <>
+            {cell.getValue()} {cell.row.original.contact.id <= 4000000 && <Badge>NPC</Badge>}
+          </>
+        );
+      },
     }),
     columnHelper.accessor("blocked", {
       header: "Blocked",
+      cell: (props) => {
+        return <BooleanCheckBox checked={props.getValue()} />;
+      },
     }),
     columnHelper.accessor("watched", {
       header: "Watching",
+      cell: (props) => {
+        return <BooleanCheckBox checked={props.getValue()} />;
+      },
     }),
     columnHelper.accessor("standing", {
       header: "Standing",
@@ -36,9 +53,30 @@ const CharacterContacts = () => {
     }),
   ];
 
+  const data_filtered = data?.filter((row: any) => {
+    if (!showNPC) {
+      return row.contact.id > 4000000;
+    } else {
+      return true;
+    }
+  });
+
   return (
     <>
-      <TableWrapper {...{ data, isFetching, columns }} />
+      <Card.Header className="text-end">
+        <div className="d-flex justify-content-end">
+          <Form.Check // prettier-ignore
+            type="switch"
+            id="custom-switch"
+            label="Show NPC Contacts"
+            onChange={(event) => {
+              setShowNPC(event.target.checked);
+            }}
+            defaultChecked={showNPC}
+          />
+        </div>
+      </Card.Header>
+      <TableWrapper data={data_filtered} {...{ isFetching, columns }} />
     </>
   );
 };
