ef416346be294da83c3cb140cb1b89d66271495d
Aaron Kable <aaronkable@gmail.com>
2025-02-18 17:49:22 +0800

    Add modal for implant details

--- frontend/frontend/src/Pages/Char/Clones.tsx
+++ frontend/frontend/src/Pages/Char/Clones.tsx
@@ -3,14 +3,17 @@ import TableWrapper from "../../Components/Tables/BaseTable/TableWrapper";
 import { components } from "../../api/CtApi";
 import { getCharacterClones } from "../../api/character";
 import { createColumnHelper } from "@tanstack/react-table";
-import { Table } from "react-bootstrap";
+import { Button, Modal, Table } from "react-bootstrap";
 import { useQuery } from "react-query";
 import { useParams } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import React from "react";
 
 const CharacterClones = () => {
   const { t } = useTranslation();
   const { characterID } = useParams();
+  const [openModal, setOpenModal] = React.useState(false);
+  const [modalData, setModalData] = React.useState(null as any);
 
   const { data, isFetching } = useQuery({
     queryKey: ["clones", characterID],
@@ -35,43 +38,104 @@ const CharacterClones = () => {
     columnHelper.accessor("clones", {
       header: t("Clones"),
       cell: (props) => {
+        const data = props.getValue();
         return (
-          <Table>
-            <thead>
-              <th>{t("Location")}</th>
-              <th className="text-end">{t("Implants")}</th>
-            </thead>
-            <tbody>
-              {props.getValue()?.map((d: components["schemas"]["CharacterClone"]) => {
-                return (
-                  <tr>
-                    <td>
-                      {d.name} {d.location?.name}
-                    </td>
-                    <td className="">
-                      <div className="d-flex justify-content-end align-items-center">
+          <>
+            <Table>
+              <thead>
+                <th>{t("Location")}</th>
+                <th className="text-end pe-2">{t("Implants")}</th>
+                <th style={{ width: "45px" }}></th>
+              </thead>
+              <tbody>
+                {data?.map((d: components["schemas"]["CharacterClone"]) => {
+                  return (
+                    <tr className="align-items-center">
+                      <td style={{ verticalAlign: "middle" }}>
+                        {d.name} {d.location?.name}
+                      </td>
+                      <td style={{ verticalAlign: "middle" }}>
+                        <div className="d-flex justify-content-end align-items-center">
+                          {d.implants?.length ? (
+                            d.implants.map((imp) => {
+                              return <TypeIcon textContent={imp.name} type_id={imp.id} size={32} />;
+                            })
+                          ) : (
+                            <i className="fa-regular fa-circle-xmark pe-2"></i>
+                          )}
+                        </div>
+                      </td>
+                      <td style={{ verticalAlign: "middle" }}>
                         {d.implants?.length ? (
-                          d.implants.map((imp) => {
-                            return <TypeIcon textContent={imp.name} type_id={imp.id} size={32} />;
-                          })
+                          <Button
+                            variant="primary"
+                            size="sm"
+                            onClick={() => {
+                              setModalData(d);
+                              setOpenModal(true);
+                            }}
+                          >
+                            <i className="fa-solid fa-info"></i>
+                          </Button>
                         ) : (
-                          <i className="fa-regular fa-circle-xmark"></i>
+                          <></>
                         )}
-                      </div>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
+                      </td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </Table>
+          </>
         );
       },
     }),
   ];
+  console.log(modalData);
 
   return (
     <>
       <TableWrapper {...{ data, isFetching, columns }} />
+      {modalData?.implants && (
+        <Modal
+          size="lg"
+          show={openModal}
+          onHide={() => {
+            setOpenModal(false);
+          }}
+        >
+          <Modal.Header closeButton>{t(`Implant Details`)}</Modal.Header>
+          <Modal.Body>
+            <div>
+              <Table striped style={{ marginBottom: 0 }}>
+                <tbody>
+                  {modalData?.implants.map((imp: any) => {
+                    return (
+                      <tr key={imp.name}>
+                        <td className="text-end">
+                          <TypeIcon textContent={imp.name} type_id={imp.id} size={32} />
+                        </td>
+                        <td className="text-start">{imp.name}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </Table>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button
+              variant="secondary"
+              className="w-100"
+              onClick={() => {
+                setOpenModal(false);
+              }}
+            >
+              {t("Close")}
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      )}
     </>
   );
 };
