af21dc80465cceeec7724f330de67ff37ffa1fdd
Aaron Kable <aaronkable@gmail.com>
2025-03-02 11:06:15 +0800

    Errors with info!

--- frontend/frontend/src/Components/Helpers/ErrorBoundary.tsx
+++ frontend/frontend/src/Components/Helpers/ErrorBoundary.tsx
@@ -7,22 +7,43 @@ interface Props {
 
 interface State {
   hasError: boolean;
+  title?: string;
+  message?: string;
+  trace?: string;
 }
 
 class ErrorBoundary extends Component<Props, State> {
   constructor(props: Props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = {
+      hasError: false,
+      message: "",
+      trace: "",
+    };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     console.error("ErrorBoundary caught an error: ", error, errorInfo);
-    this.setState({ hasError: true });
+    this.setState({
+      hasError: true,
+      title: error.name,
+      message: error.message,
+      trace: String(errorInfo.componentStack),
+    });
   }
 
   render() {
     if (this.state.hasError) {
-      return <ErrorLoader />;
+      return (
+        <>
+          <ErrorLoader title={this.state.title} message={this.state.message} />
+          <div style={{ justifyContent: "center" }} className="d-flex">
+            <pre style={{ maxWidth: "1000px" }} className="border">
+              {this.state.trace}
+            </pre>
+          </div>
+        </>
+      );
     }
 
     return this.props.children;
