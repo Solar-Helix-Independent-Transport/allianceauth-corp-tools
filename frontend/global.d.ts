a831cae8d427e595a0c7724740d56bc4e559f1f3
Aaron Kable <aaronkable@gmail.com>
2025-04-05 13:25:11 +0800

    add custom type...

--- frontend/global.d.ts
+++ frontend/global.d.ts
@@ -2,3 +2,22 @@ declare module "*.module.css" {
   const content: Record<string, string>;
   export default content;
 }
+
+declare module "react-copy-to-clipboard" {
+  import React from "react";
+
+  interface Options {
+    debug: boolean;
+    message: string;
+  }
+
+  interface Props {
+    text: string;
+    onCopy?(a: string, b: boolean): void;
+    options?: Options;
+    children?: ReactNode;
+  }
+
+  class CopyToClipboard extends React.Component<PropsWithChildren<Props>, {}> {}
+  export default CopyToClipboard;
+}
