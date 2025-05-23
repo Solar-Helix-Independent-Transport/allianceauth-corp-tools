12b9497b57e4cc1110ff460c0990825d9c4400b1
Aaron Kable <aaronkable@gmail.com>
2025-05-23 17:33:37 +0800

    vbump

--- corptools/__init__.py
+++ corptools/__init__.py
@@ -1,5 +1,5 @@
 """
 This module is the core of the CorpTools Ecosystem
 """
-__version__ = "2.10.5"
+__version__ = "2.10.6"
 __url__ = "https://github.com/Solar-Helix-Independent-Transport/allianceauth-corp-tools"
