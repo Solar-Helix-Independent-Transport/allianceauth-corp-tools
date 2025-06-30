1f0dfd6d6139e5f72671330e5d45a2bab919b71a
Peter Pfeufer <ppfeufer@users.noreply.github.com>
2025-06-30 14:06:42 +0200

    [CHANGE] Make states for `guest_hr` permission configurable (#219)
    - Default to `["Guest"]` when not set, to not change the established behaviour
    
    This is useful when more than one state could be considered "Guest", as in "Not Member" but still valid for applications.

--- corptools/app_settings.py
+++ corptools/app_settings.py
@@ -5,6 +5,8 @@ CORPTOOLS_APP_NAME = getattr(settings, "CORPTOOLS_APP_NAME", "Character Audit")
 CORPTOOLS_DISCORD_BOT_COGS = getattr(
     settings, 'CORPTOOLS_DISCORD_BOT_COGS', ["corptools.cogs.routes", "corptools.cogs.locate", "corptools.cogs.where"])
 
+CORPTOOLS_GUEST_HR_STATES = getattr(settings, 'CORPTOOLS_GUEST_HR_STATES', ["Guest"])
+
 CT_CHAR_MAX_INACTIVE_DAYS = getattr(settings, 'CT_CHAR_MAX_INACTIVE_DAYS', 3)
 
 CT_CHAR_ASSETS_MODULE = getattr(settings, 'CT_CHAR_ASSETS_MODULE', True)
