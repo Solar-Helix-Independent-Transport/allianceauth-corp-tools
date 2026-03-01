6218f53fcbd329001e84525c778948534a8f8f9f
Aaron Kable <aaronkable@gmail.com>
2026-03-01 11:57:14 +0800

    fix fittings skill checker templattag

--- corptools/templatetags/fittings_skill_tree.py
+++ corptools/templatetags/fittings_skill_tree.py
@@ -1,5 +1,5 @@
 # Third Party
-from eve_sde.models import DogmaAttribute, ItemType
+from eve_sde.models import ItemType, TypeDogma
 
 # Django
 from django import template
@@ -30,30 +30,34 @@ def character_skill_overview(context) -> dict:
     _skill_ids = [182, 183, 184, 1285, 1289, 1290]
     _level_ids = [277, 278, 279, 1286, 1287, 1288]
 
-    _types = DogmaAttribute.objects.filter(
-        eve_type_id__in=_items, attribute_id__in=_skill_ids + _level_ids)
-    _types = _types | DogmaAttribute.objects.filter(
-        eve_type_id=_fit.ship_type_type_id, attribute_id__in=_skill_ids + _level_ids)
+    _types = TypeDogma.objects.filter(
+        item_type_id__in=_items,
+        dogma_attribute_id__in=_skill_ids + _level_ids
+    )
+    _types = _types | TypeDogma.objects.filter(
+        item_type_id=_fit.ship_type_type_id,
+        dogma_attribute_id__in=_skill_ids + _level_ids
+    )
 
     required = {}
     skills = {}
     sids = set()
     for t in _types:
-        if t.eve_type_id not in required:
-            required[t.eve_type_id] = {0: {"skill": 0, "level": 0},
-                                       1: {"skill": 0, "level": 0},
-                                       2: {"skill": 0, "level": 0},
-                                       3: {"skill": 0, "level": 0},
-                                       4: {"skill": 0, "level": 0},
-                                       5: {"skill": 0, "level": 0}}
-        a = t.attribute_id
+        if t.item_type_id not in required:
+            required[t.item_type_id] = {0: {"skill": 0, "level": 0},
+                                        1: {"skill": 0, "level": 0},
+                                        2: {"skill": 0, "level": 0},
+                                        3: {"skill": 0, "level": 0},
+                                        4: {"skill": 0, "level": 0},
+                                        5: {"skill": 0, "level": 0}}
+        a = t.dogma_attribute_id
         v = t.value
         if a in _skill_ids:
-            required[t.eve_type_id][_skill_ids.index(a)]["skill"] = v
+            required[t.item_type_id][_skill_ids.index(a)]["skill"] = v
         elif a in _level_ids:
             indx = _level_ids.index(a)
-            if required[t.eve_type_id][indx]["level"] < v:
-                required[t.eve_type_id][indx]["level"] = v
+            if required[t.item_type_id][indx]["level"] < v:
+                required[t.item_type_id][indx]["level"] = v
 
         for t in required.values():
             for s in t.values():
@@ -66,9 +70,9 @@ def character_skill_overview(context) -> dict:
     sk_check = {
 
     }
-    for t in ItemType.objects.filter(type_id__in=list(sids)):
-        skills[t.type_id]["n"] = t.name
-        sk_check[t.name] = skills[t.type_id]["l"]
+    for t in ItemType.objects.filter(id__in=list(sids)):
+        skills[t.id]["n"] = t.name
+        sk_check[t.name] = skills[t.id]["l"]
 
     # Standard Library
     import json
