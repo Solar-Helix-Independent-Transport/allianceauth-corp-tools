a4a94248d06d1ea7838d3f9ec2a9ffb69247b11d
Aaron Kable <aaronkable@gmail.com>
2026-06-11 18:27:21 +0800

    fix crash on no main

--- frontend/src/Components/Character/CharacterHeader.tsx
+++ frontend/src/Components/Character/CharacterHeader.tsx
@@ -50,26 +50,26 @@ const CharHeader = () => {
         <CharacterPortrait
           style={{ borderRadius: "0.375rem 0 0 0.375rem" }}
           className="m-0"
-          character_id={data?.main.character_id}
+          character_id={data?.main?.character_id}
           size={64}
         />
-        <h4 className="m-1 mx-3">{data?.main.character_name}</h4>
+        <h4 className="m-1 mx-3">{data?.main?.character_name}</h4>
         <CorporationLogo
           style={style}
           className="m-1 mx-3"
-          corporation_id={data?.main.corporation_id}
+          corporation_id={data?.main?.corporation_id}
           size={32}
         />
-        <h5 className="m-1 mx-3">{data?.main.corporation_name}</h5>
-        {data?.main.alliance_id && (
+        <h5 className="m-1 mx-3">{data?.main?.corporation_name}</h5>
+        {data?.main?.alliance_id && (
           <>
             <AllianceLogo
               style={style}
               className="m-1 mx-3"
-              alliance_id={data?.main.alliance_id}
+              alliance_id={data?.main?.alliance_id}
               size={32}
             />
-            <h5 className="m-1 mx-3">{data?.main.alliance_name}</h5>
+            <h5 className="m-1 mx-3">{data?.main?.alliance_name}</h5>
           </>
         )}
         <ButtonGroup className="me-3 ms-auto">
